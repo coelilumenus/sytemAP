f5810bbbdc7b2b85c15a8d59ab3a7b357b64a718
Valentine <war.nub@yandex.ru>
2022-05-02 18:27:08 +0300

    clear empty layout

--- src/layouts/AppLayoutEmpty.vue
+++ src/layouts/AppLayoutEmpty.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <AppLayoutLinks />
     <slot />
   </div>
 </template>
 
 <script>
-import AppLayoutLinks from "@/layouts/AppLayoutLinks.vue"
 export default {
-  name: "AppLayoutEmpty",
-  
-  components: {
-    AppLayoutLinks,
-  },
+  name: "AppLayoutEmpty"
 }
 </script>
