bdb18c4718489fb173b1412e48e748da318860d7
coelilumenus <war.nub@yandex.ru>
2022-05-07 21:48:38 +0300

    clear layout

--- src/components/layout/LayoutHeader.vue
+++ src/components/layout/LayoutHeader.vue
@@ -1,22 +1,7 @@
 <template>
-  <div class="bg-gray-200 w-full h-20 flex p-4">
-    <div>
-      <button
-        class="w-10 h-10 flex justify-center items-center rounded-md hover:bg-gray-300 active:bg-gray-400"
-        @click="$emit('changeSidebarWide')"
-      >
-        <menu-icon class="text-gray-600 fill-current" />
-      </button>
-    </div>
-  </div>
+  <div class="w-full h-20 flex p-4"></div>
 </template>
 
 <script>
-import MenuIcon from "@/assets/icons/menu.svg"
-
-export default {
-  components: {
-    MenuIcon,
-  },
-}
+export default {};
 </script>
